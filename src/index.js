135b2392bdcfd4a404fab7a72ecf8ff3cd5597cb
Khloptsev Petr <petr.khloptsev@fogstream.ru>
2023-11-09 13:18:37 +1000

    add slider with swipper

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
-import 'normalize.css'
+import 'normalize.css';
 import './styles/main.scss';
+
+import Swiper from 'swiper/bundle';
+
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  autoplay: {
+    delay: 5000,
+  },
+  speed: 1000,
+});
