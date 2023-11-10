b12d1d02400dc65233d95ca75c0b51b05a2badb9
Khloptsev Petr <petr.khloptsev@fogstream.ru>
2023-11-10 15:26:39 +1000

    add adaptive design

--- src/index.js
+++ src/index.js
@@ -15,12 +15,6 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination',
   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
@@ -30,4 +24,60 @@ const swiper = new Swiper('.swiper', {
     delay: 5000,
   },
   speed: 1000,
+  breakpoints: {
+    1000: {
+      navigation: {
+        enabled: true,
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    },
+    370: {
+      navigation: {
+        enabled: false,
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    },
+  },
 });
+
+// const hamb = document.querySelector('#hamb');
+// const popup = document.querySelector('#popup');
+// const body = document.body;
+
+// // Клонируем меню, чтобы задать свои стили для мобильной версии
+// const menu = document.querySelector('#menu').cloneNode(1);
+
+// // При клике на иконку hamb вызываем ф-ию hambHandler
+// hamb.addEventListener('click', hambHandler);
+
+// // Выполняем действия при клике ..
+// function hambHandler(e) {
+//   e.preventDefault();
+//   // Переключаем стили элементов при клике
+//   popup.classList.toggle('open');
+//   hamb.classList.toggle('active');
+//   body.classList.toggle('noscroll');
+//   renderPopup();
+// }
+
+// // Здесь мы рендерим элементы в наш попап
+// function renderPopup() {
+//   popup.appendChild(menu);
+// }
+
+// // Код для закрытия меню при нажатии на ссылку
+// const links = Array.from(menu.children);
+
+// // Для каждого элемента меню при клике вызываем ф-ию
+// links.forEach((link) => {
+//   link.addEventListener('click', closeOnClick);
+// });
+
+// // Закрытие попапа при клике на меню
+// function closeOnClick() {
+//   popup.classList.remove('open');
+//   hamb.classList.remove('active');
+//   body.classList.remove('noscroll');
+// }
